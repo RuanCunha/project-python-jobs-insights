03ef1218fd9a5ce16a7f7a884e25f06c39719630
RuanCunha <ruanvcunha@hotmail.com>
2022-08-04 19:28:59 -0300

    Requisito 8 finalizado

--- src/insights.py
+++ src/insights.py
@@ -170,7 +170,15 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    try:
+        if int(job["max_salary"]) < int(job["min_salary"]):
+            raise ValueError('Valor invalido')
+
+        return int(job["min_salary"]) <= salary <= int(job["max_salary"])
+    except KeyError:
+        raise ValueError('Min e/ou Max Salary não existe')
+    except TypeError:
+        raise ValueError('Valor invalido')
 
 
 def filter_by_salary_range(jobs, salary):
